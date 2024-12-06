3956ae4407486e6b6baf950504a507a53ef7695e
Donjosx <159622061+Donjosx@users.noreply.github.com>
2024-12-07 05:46:17 +0700

    Update LobbyName.lua

--- LobbyName.lua
+++ LobbyName.lua
@@ -1,1 +1,1 @@
-return game.Workspace.Lobby, game:GetService("Workspace").Lobby.Teleport1
+return game.Workspace.Lobby, game:GetService("Workspace").Lobby.Teleport1, game:GetService("Workspace").Lobby.brazil.portal
